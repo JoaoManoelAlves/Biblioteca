47acf4eeb8b59da37553fd0cbfab184cdda41ea6
JoaoManoelAlves <joaomanoel88642@gmail.com>
2026-05-20 16:15:26 -0300

    Modificações nos routes

--- start/routes.ts
+++ start/routes.ts
@@ -2,7 +2,9 @@ import { controllers } from '#generated/controllers'
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.ts'
 
-//router.post('/user', [controllers.Users, 'store'])
+/*router.get('/user', [controllers.Users, 'index'])
+router.post('/user', [controllers.Users, 'store'])
+router.delete('/user/:id', [controllers.Users, 'destroy'])*/
 router.post('/session', [controllers.AccessTokens, 'store'])
 router.delete('/session', [controllers.AccessTokens, 'destroy'])
 router
