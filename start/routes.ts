97e62739cdb0bfc09a36adbe94750b2072f5cdbd
JoaoManoelAlves <162204418+JoaoManoelAlves@users.noreply.github.com>
2026-05-23 19:41:22 -0300

    Rename session routes to login/logout

--- start/routes.ts
+++ start/routes.ts
@@ -5,11 +5,11 @@ import { middleware } from './kernel.ts'
 /*router.get('/user', [controllers.Users, 'index'])
 router.post('/user', [controllers.Users, 'store'])
 router.delete('/user/:id', [controllers.Users, 'destroy'])*/
-router.post('/session', [controllers.AccessTokens, 'store'])
-router.delete('/session', [controllers.AccessTokens, 'destroy'])
+router.post('/login', [controllers.AccessTokens, 'store'])
+router.delete('/logout', [controllers.AccessTokens, 'destroy'])
 router
   .group(() => {
-    router.resource('/user', controllers.Users)
+    router.resource('/users', controllers.Users)
     router.resource('/books', controllers.Books)
   })
   .use(middleware.auth())
